242f83925bafd55b00d309132107a8735b87fdea
Leonardo Schneider <lnrdschneider@gmail.com>
2020-10-27 20:33:35 -0300

    about structure, carousel size, background and transparency

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+    const div = document.createElement('div');
+    // div.innerHTML = "Test"
+    div.style.color = "blue";
+    document.body.appendChild(div);
+  }
+
+$('.carousel').carousel()
