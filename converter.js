9d251581e0b19b99c695f304f4f514ce34b79e6d
Leonardo Schneider <lnrdschneider@gmail.com>
2021-01-12 19:53:01 -0300

    feat: Reynolds Number calculator (not implemented yet)

---
+++ converter.js
@@ -0,0 +1,40 @@
+let V = null;
+let L = null;
+let d = null;
+let p = null;
+
+function calcRe() {
+  
+
+  V = document.getElementById("inputVelocity").value;
+  console.log("V = ",V);
+
+  L = document.getElementById("inputDiameter").value;
+  console.log("L = ",L);
+
+  d = document.getElementById("inputDensity").value;
+  console.log("d = ",d);
+
+  p = document.getElementById("inputViscosity").value;
+  console.log("p = ",p);
+
+   // m/l cm/s ft/s mph in/s
+   // m in ft cm nm
+   // kg/m^3 lb/in^3 kg/l
+   // cP Pa-s kb/m-s slug/ft-s
+  
+  
+  document.getElementById("outputRe").innerHTML=(V*L*d)/p;
+}
+
+// // abaixo de 2000
+// Plate flow is still laminar at a distance D from the leading edge.
+// Pipe flow is laminar in a pipe of diameter D.
+
+// // acima de 2000
+// Plate flow is still laminar at a distance D from the leading edge.
+// Pipe flow is transitioning to turbulent in a pipe of diameter D.
+
+// // acima de 10000
+// Plate flow is transitioning to turbulent at a distance D from the leading edge.
+// Pipe flow is fully turbulent in a pipe of diameter D.
