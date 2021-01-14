a430df4e2deb15158d88457292c4fb3d6a00ed36
Leonardo Schneider <lnrdschneider@gmail.com>
2021-01-14 13:44:32 -0300

    begin to write phrases of types of flow in a conditional

--- calc.js
+++ calc.js
@@ -1,3 +1,5 @@
+const { divide } = require("mathjs");
+
 function convertUnits() {
   // Get the input values
   V = document.getElementById("inputVelocity").value;
@@ -6,27 +8,57 @@ function convertUnits() {
   p = document.getElementById("inputViscosity").value;
 
   // Velocity units converting
-  var veloc = document.getElementById("unitV").value;
+  const veloc = document.getElementById("unitV").value;
   v1 = V / veloc;
 
   // // Length units converting
-  var len = document.getElementById("unitL").value;
+  const len = document.getElementById("unitL").value;
   l1 = L / len;
 
   // // Density units converting
-  var den = document.getElementById("unitD").value;
+  const den = document.getElementById("unitD").value;
   d1 = d / den;
 
   // // Viscosity units converting
-  var visc = document.getElementById("unitP").value;
+  const visc = document.getElementById("unitP").value;
   p1 = p / visc;
 
+  // Reynolds number
+  const reyNum = ((v1 * l1 * d1) / p1);
+
   // Print the answer on the website screen
-  if (isNaN((v1 * l1 * d1) / p1)) {
-    var x = document.getElementById("outputRe").innerHTML = "";
+  if (isNaN(reyNum)) {
+    let x = document.getElementById("outputRe").innerHTML = "";
   } else {
-    var x = document.getElementById("outputRe").innerHTML = ((v1 * l1 * d1) / p1);
+    let x = document.getElementById("outputRe").innerHTML = (reyNum);
   }
+
+  // Conditional text for the flow (laminar, transition, turbulent)
+  // if (reyNum <= 2000) {
+  //   const newDiv = document.createElement("div");
+  //   const newContent = document.createTextNode(
+  //     "Pipe flow is laminar in a pipe of diameter D."
+  //     );
+  //   newDiv.appendChild(newContent);
+  //   const currentDiv = document.getElementById("div1");
+  //     document.body.insertBefore(newDiv, currentDiv);
+  // } else if (2000 < reyNum < 2900) {
+  //   const newDiv = document.createElement("div");
+  //   const newContent = document.createTextNode(
+  //     "Pipe flow is transitioning to turbulent in a pipe of diameter D."
+  //   );
+  //   newDiv.appendChild(newContent);
+  //   const currentDiv = document.getElementById("div1");
+  //   document.body.insertBefore(newDiv, currentDiv);
+  // } else {
+  //   const newDiv = document.createElement("div");
+  //   const newContent = document.createTextNode(
+  //     "Pipe flow is fully turbulent in a pipe of diameter D."
+  //   );
+  //   newDiv.appendChild(newContent);
+  //   const currentDiv = document.getElementById("div1");
+  //   document.body.insertBefore(newDiv, currentDiv);
+  // }
 }
 
 // // abaixo de 2000
@@ -37,6 +69,6 @@ function convertUnits() {
 // Plate flow is still laminar at a distance D from the leading edge.
 // Pipe flow is transitioning to turbulent in a pipe of diameter D.
 
-// // acima de 10000
+// // acima de 2900
 // Plate flow is transitioning to turbulent at a distance D from the leading edge.
 // Pipe flow is fully turbulent in a pipe of diameter D.
