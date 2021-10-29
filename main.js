db834af252bdbed37877c4e0db8a07b469ac578f
Leonardo Schneider <lnrdschneider@gmail.com>
2021-10-29 17:31:09 -0300

    mobile version updated

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-$('.carousel').carousel()
+$('.carousel').carousel();
+
+$(window)
+  .resize(function () {
+    var mobileWidth = window.innerWidth > 0 ? window.innerWidth : screen.width;
+    var viewport =
+      mobileWidth > 300
+        ? 'width=device-width, initial-scale=1.0'
+        : 'width=1900';
+    $('meta[name=viewport]').attr('content', viewport);
+  })
+  .resize();
