46f68c71ec0999e124d4f63aad2674e35e4c67c1
Leonardo Schneider <lnrdschneider@gmail.com>
2021-12-14 13:19:24 -0300

    clean the unneed comments

--- js.js
+++ js.js
@@ -1,14 +1,4 @@
 function currentDate() {
-  // var today = new Date();
-  // var dd = String(today.getDate()).padStart(2, '0');
-  // var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-  // var yyyy = today.getFullYear();
-
-  // today = mm + '/' + dd + '/' + yyyy;
-
-  // var message = `Last Updated on ${today}`;
-  // document.getElementById('updateFooter').innerHTML = message;
-
   fetch('date.txt')
     .then((response) => response.text())
     .then((data) => {
